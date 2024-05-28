7e1ed7fe95e4c6a565e744ba88f5f63f68518efe
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-05-28 14:32:41 +0100

    Creates model and controller for /api endpoint

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,4 +1,12 @@
-const { fetchAllTopics } = require("../models/topics.models");
+const {
+  fetchAllEndpoints,
+  fetchAllTopics,
+} = require("../models/topics.models");
+
+exports.getAllEndpoints = (req, res) => {
+  const allEndpoints = fetchAllEndpoints();
+  res.status(200).send({ endpoints: allEndpoints });
+};
 
 exports.getAllTopics = (req, res) => {
   fetchAllTopics().then(({ rows }) => {
--- models/topics.models.js
+++ models/topics.models.js
@@ -1,4 +1,9 @@
 const db = require('../db/connection');
+const endpoints = require('../endpoints.json')
+
+exports.fetchAllEndpoints = () => {
+  return endpoints;
+}
 
 exports.fetchAllTopics = () => {
   return db.query('SELECT * FROM topics');
