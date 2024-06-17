b7c3452109bda0fab64f9e54093fbd72e72424d9
ZviHaffner <zvihaffner@Zvi-Haffners-Laptop>
2024-06-17 10:42:22 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 const apiRouter = require("./routers/api-router");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
