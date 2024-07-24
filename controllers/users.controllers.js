39f86aebc3c9434de816b954882f61f172920d95
Zvi Haffner <zvihaffner@gmail.com>
2024-07-24 17:52:04 +0100

    Adds functionality to get a user by username

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,7 +1,19 @@
-const { fetchAllUsers } = require("../models/users.models");
+const {
+  fetchAllUsers,
+  fetchUserByUsername,
+} = require("../models/users.models");
 
 exports.getAllUsers = (req, res) => {
   fetchAllUsers().then(({ rows }) => {
     res.status(200).send({ users: rows });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  fetchUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch(next);
 };
